5fa0293da14b795c77303e5c760e3d9902da7fcb
BArFinrod <68872465+BArFinrod@users.noreply.github.com>
2023-01-23 13:56:31 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,16 +1,8 @@
-# import streamlit as st
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# plt.plot([1,2,3,4],[1,2,2,1])
-# plt.show()
-
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot([1,2,3,4],[1,2,2,1])
-plt.show()
+st.pyplot(plt.plot([1,2,3,4],[1,2,2,1]))
 
 # arr = np.random.normal(1, 1, size=100)
 # fig, ax = plt.subplots()
