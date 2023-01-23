0d99ec9849a19310a579b088dbe1e752d3137a60
BArFinrod <68872465+BArFinrod@users.noreply.github.com>
2023-01-23 13:54:07 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,16 @@
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+# import streamlit as st
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4],[1,2,2,1])
-plt.show()
+# plt.plot([1,2,3,4],[1,2,2,1])
+# plt.show()
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
